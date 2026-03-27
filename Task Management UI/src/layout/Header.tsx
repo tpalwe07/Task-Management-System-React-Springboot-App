72b1878475f8c108fdea91ee59eac1a19c0c5f24
Patil, Aakash <aakash.patil@gmail.com>
2026-03-28 00:29:17 +0530

    PCTM-1 feat:added sidebar comp

--- Task Management UI/src/layout/Header.tsx
+++ Task Management UI/src/layout/Header.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { COLORS } from '../theme/theme';
 
 const Header: React.FC = () => (
-  <AppBar position='static'>
+  <AppBar position='static' sx={{ bgcolor: COLORS.primaryMain }}>
     <Toolbar>
       <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
         Task Management System
--- Task Management UI/src/layout/Layout.tsx
+++ Task Management UI/src/layout/Layout.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Header from './Header';
 import Footer from './Footer';
+import Sidebar from './Sidebar';
 
 const Layout: React.FC = () => (
   <Box display='flex' flexDirection='column' minHeight='100vh'>
     <Header />
-    <Container sx={{ flex: 1, py: 4 }} maxWidth='lg'>
-      <Outlet />
-    </Container>
+    <Box display='flex' flex={1}>
+      <Sidebar />
+      <Box component='main' sx={{ flex: 1, p: 3, overflowY: 'auto' }}>
+        <Outlet />
+      </Box>
+    </Box>
     <Footer />
   </Box>
 );
